f8d5a99333e96f9215fb455588b720e43faca114
nattlip <nattelip@hotmail.com>
2017-07-09 12:08:00 +0200

    first admission github
    node javascript file for serial to net stream connection, to use a
    serial (usb) port  via a net server client connection.

---
+++ README.md
@@ -0,0 +1,38 @@
+﻿# Serial2Net
+
+
+
+This file can be run in node.js to convert a serial stream to net stream and vice versa 
+Is is used on a raspberry pi 3 with debian os and node 4.8.3 to connect a serial usb device to a net stream.
+the long existing Serial2Net app didnt work for me on a raspberry pi 3 
+
+## changeLog
+
+09-07-20127 
+
+first admission to github
+
+
+
+
+[![Paypal donate][pp-donate-image]][pp-donate-link]
+[pp-donate-link]: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=42UGL52J4KPZE
+[pp-donate-image]: https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif
+
+
+
+
+
+
+Copyright (c) 2017 Jilles Miedema
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+Contact GitHub API Training Shop Blog About
+© 2016 GitHub, Inc. Terms Privacy Security Status Help
+
+
+
---
+++ serial2net.js
@@ -0,0 +1,121 @@
+﻿
+'use strict';
+
+
+
+var util = require("util")
+
+
+
+const buffer = require('buffer');
+var serialport = require('serialport');
+var SerialPort = serialport.SerialPort;
+
+// list serial ports:
+serialport.list(function (err, ports) {
+    ports.forEach(function (port) {
+        console.log(port.comName);
+    });
+});
+
+
+
+var port = new SerialPort('/dev/ttyUSB0', {
+    encoding: 'hex', baudRate: 38400, dataBits: 8, stopBits: 1, parity: 'none',
+});
+
+
+
+
+port.on('open', function () {
+
+    console.log('port opening');
+
+});
+
+
+
+
+//port.open(function (err) {
+//    if (err) {
+//        return console.log('Error opening port: ', err.message);
+//    }
+
+//    // write errors will be emitted on the port since there is no callback to write 
+//    // port.write('main screen turn on');
+//});
+
+
+
+
+
+port.on('data', function (data) {
+    console.log('Data: ' + data);
+
+    var myString = JSON.stringify(data)
+    console.log('second Data: ' + myString);
+
+    //port.pipe(socket)
+    if (socket)
+    { socket.write(data) }
+
+    console.log('string hex ', data.toString('hex'));
+});
+
+port.on('error', err => {
+
+    console.log('error port', err)
+
+})
+
+
+
+
+var socket = false
+
+//https://gist.github.com/creationix/707146
+
+const net = require('net');
+const server = net.createServer((c) => {  // c is socket connecting
+    // 'connection' listener
+
+    socket = c
+    console.log('client connected');
+    c.on('end', () => {
+        socket = false
+        console.log('client disconnected');
+        c.destroy()
+    });
+
+
+    c.on('data', data => {
+
+        console.log('data from client', data)
+
+        port.write(data)
+
+    })
+
+    c.on('error', err => {
+
+        console.log(' client error ', err)
+        socket = false
+        c.end()
+        c.destroy()
+    })
+
+
+
+
+
+
+});
+server.on('error', (err) => {
+    socket = false
+    console.log('error server', err)
+});
+server.listen(10001, () => {
+    console.log('server bound');
+});
+
+
